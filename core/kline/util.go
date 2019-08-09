4c43c7248cb7d4f32bba6b8f2d3f4ee8ee62c172
Eric Zhang <eric.pucker@gmail.com>
2019-08-09 11:37:28 +0800

    add kline module

---
+++ core/kline/util.go
@@ -0,0 +1,15 @@
+package kline
+
+func maxFloat(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func minFloat(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
