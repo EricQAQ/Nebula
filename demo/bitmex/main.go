e1d5e60cf2fc4d2c073ac3e64551d0aa663c3271
Eric Zhang <eric.pucker@gmail.com>
2019-08-13 00:12:37 +0800

    add a simple strategy

--- demo/bitmex/main.go
+++ demo/bitmex/main.go
@@ -35,9 +35,7 @@ func main() {
 				continue
 			}
 			if isUpdate {
-				log.Infof(`
-Receive tick data:
-symbol: %s, open: %f, close: %f, high: %f, low: %f, vol: %f, time: %s`,
+				log.Infof("Receive tick data: symbol: %s, open: %f, close: %f, high: %f, low: %f, vol: %f, time: %s",
 					tick.Symbol, tick.Open, tick.Close, tick.High,
 					tick.Low, tick.Vol, tick.Timestamp)
 			}
@@ -50,10 +48,7 @@ symbol: %s, open: %f, close: %f, high: %f, low: %f, vol: %f, time: %s`,
 			positions, isUpdate := bm.GetPosition("XBTUSD")
 			if isUpdate {
 				for _, pos := range positions {
-					log.Infof(`
-Position: leverage:%f,
-sell_amount:%f, sell_avaiable:%f, sell_price_avg:%f, sell_profit_real:%f
-buy_amount:%f, buy_avaiable:%f, buy_price_avg:%f, buy_profit_real:%f`,
+					log.Infof("Position: leverage:%f, sell_amount:%f, sell_avaiable:%f, sell_price_avg:%f, sell_profit_real:%f buy_amount:%f, buy_avaiable:%f, buy_price_avg:%f, buy_profit_real:%f",
 						pos.LeverRate, pos.SellAmount, pos.SellAvailable, pos.SellPriceAvg, pos.SellProfitReal,
 						pos.BuyAmount, pos.BuyAvailable, pos.BuyPriceAvg, pos.BuyProfitReal)
 				}
@@ -65,9 +60,12 @@ buy_amount:%f, buy_avaiable:%f, buy_price_avg:%f, buy_profit_real:%f`,
 	go func() {
 		for {
 			for _, interval := range app.Cfg.KlineInterval {
-				kline, isUpdate := app.GetKline("bitmex", "XBTUSD", interval)
+				klist, isUpdate := app.GetKline("bitmex", "XBTUSD", interval)
+				kline := klist[len(klist)-1]
 				if isUpdate {
-					log.Infof("K line %d, symbol:%s, open:%f, close:%f, high:%f, low:%f, vol:%f, time:%s", interval, kline.Symbol, kline.Open, kline.Close, kline.High, kline.Low, kline.Vol, kline.Timestamp)
+					log.Infof(
+						"K line %d, symbol:%s, open:%f, close:%f, high:%f, low:%f, vol:%f, time:%s",
+						interval, kline.Symbol, kline.Open, kline.Close, kline.High, kline.Low, kline.Vol, kline.Timestamp)
 				}
 			}
 			time.Sleep(time.Duration(500) * time.Millisecond)
---
+++ strategy/cross/cross.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"time"
+
+	"github.com/EricQAQ/Traed/core"
+	"github.com/EricQAQ/Traed/finIndex"
+	"github.com/EricQAQ/Traed/plugin/bitmex"
+)
+
+func createApp() *core.TraedApp {
+	app := core.NewTraedApp("bin/traed.toml")
+	bxCfg := app.Cfg.ExchangeMap["bitmex"]
+	bm := bitmex.CreateBitmex(bxCfg, app.Cfg.Http)
+	app.SetExchange("bitmex", bm)
+	return app
+}
+
+func strategy(app *core.TraedApp) {
+	klines, _ := app.GetKline("bitmex", "XBTUSD", 60)
+	ma := fin.NewMA(klines, 99)
+	ema := fin.NewEMA(klines, 17)
+	ema.Calculation()
+	ma.Calculation()
+	bm := app.Exchange["bitmex"]
+	for {
+		klines, isUpdate := app.GetKline("bitmex", "XBTUSD", 60)
+		if isUpdate {
+			ma.InsertKline(klines[len(klines)-1])
+			ema.InsertKline(klines[len(klines)-1])
+			maLen := len(ma.Points)
+			emaLen := len(ema.Points)
+			fmt.Printf(
+				"(ema, ma): (%f, %f), (%f, %f)\n",
+				ema.Points[emaLen-2].Value, ma.Points[maLen-2].Value,
+				ema.Points[emaLen-1].Value, ma.Points[maLen-1].Value)
+
+			if ema.Points[emaLen-2].Value < ma.Points[maLen-2].Value &&
+				ema.Points[emaLen-1].Value > ma.Points[maLen-1].Value {
+				fmt.Println("Buy!!")
+				depth, _ := bm.GetDepth("XBTUSD")
+				_, err := bm.LimitBuy("XBTUSD", depth.Buy[len(depth.Buy)-1].Price, 10)
+				fmt.Println("Buy!! %v", err)
+			} else if ema.Points[emaLen-2].Value > ma.Points[maLen-2].Value &&
+				ema.Points[emaLen-1].Value < ma.Points[maLen-1].Value {
+				fmt.Println("Sell!!")
+				depth, _ := bm.GetDepth("XBTUSD")
+				_, err := bm.LimitSell("XBTUSD", depth.Sell[0].Price, 10)
+				fmt.Println("Sell!! %v", err)
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func main() {
+	app := createApp()
+	if err := app.Start(); err != nil {
+		os.Exit(0)
+	}
+	go strategy(app)
+	app.Stop()
+	os.Exit(0)
+}
